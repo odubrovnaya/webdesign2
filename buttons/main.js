71acd0b70806d4d5a71ad3570bb7a0e959d071ad
olha <odubrovnaya@gmail.com>
2021-11-02 23:09:53 -0700

    Buttons JS

---
+++ buttons/main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("block-1").style.alignItems = "flex-end";
+}
+
+function myFunction2() {
+    document.getElementById("block-2").style.visibility = "hidden";
+}
+function myFunction3() {
+    document.getElementById("block-3").style.transform = "rotate(20deg)";
+}
+
+function myFunction4() {
+    document.getElementById("block-4").style.animation = "mynewmove 4s 1";
+}
+
+function myFunction5() {
+    document.getElementById("block-5").style.width = "200px";
+}
